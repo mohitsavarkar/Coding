c180eacdd726cf188bcfb40d8874eec9b2434918
mohitsavarkar <savarkarmohit77@gmail.com>
2026-07-30 20:31:43 +0530

    Remove files from main branch

--- Main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World! Your C++ setup is working perfectly." << std::endl;
-    return 0;
-}
--- embeddedc.cpp
+++
@@ -1,7 +0,0 @@
-#include <avr/io.h> // Hardware-specific registry header
-
-int main(void) {
-    DDRB |= (1 << PB5);    // Set pin 5 of Port B as an output register
-    PORTB |= (1 << PB5);   // Drive pin 5 high to turn on an LED
-    while(1);              // Superloop keeping the MCU running
-}
